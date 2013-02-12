530e13a33c45538e15b6690c1bff84940dd76abd
Fiqri Ahmad <fiqri_onomz@yahoo.com>
2013-02-12 23:53:59 +0700

    MP3 Player Java
    tambahkan library "jl 1.0.1.jar ke dalam project

---
+++ MusicPlayer/src/musicplayer/Lagu.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package musicplayer;
+
+/**
+ *
+ * @author VIQRI
+ */
+public class Lagu {
+    
+    private String idLagu;
+    private String namaLagu;
+    private String alamatLagu;
+
+    /**
+     * @return the idLagu
+     */
+    public String getIdLagu() {
+        return idLagu;
+    }
+
+    /**
+     * @param idLagu the idLagu to set
+     */
+    public void setIdLagu(String idLagu) {
+        this.idLagu = idLagu;
+    }
+
+    /**
+     * @return the namaLagu
+     */
+    public String getNamaLagu() {
+        return namaLagu;
+    }
+
+    /**
+     * @param namaLagu the namaLagu to set
+     */
+    public void setNamaLagu(String namaLagu) {
+        this.namaLagu = namaLagu;
+    }
+
+    /**
+     * @return the alamatLagu
+     */
+    public String getAlamatLagu() {
+        return alamatLagu;
+    }
+
+    /**
+     * @param alamatLagu the alamatLagu to set
+     */
+    public void setAlamatLagu(String alamatLagu) {
+        this.alamatLagu = alamatLagu;
+    }
+
+}
